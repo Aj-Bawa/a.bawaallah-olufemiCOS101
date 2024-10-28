ff0371571a40cfb9c12f6ced178d173fac10bad0
Aj-Bawa <anjolaoluwa.bawaallah-olufemi@pau.edu.ng>
2024-10-28 11:59:31 +0100

    My third commit

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = ' '; // Default
+    let alphabet: char = 'B';
+    let surname = "Bawaallah-Olufemi";
+    let first_name = "Anjolaoluwa";
+    let middle_name = "Adedayo";
+
+    println!("");
+    println!("Special character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}", surname, middle_name, first_name);
+}
---
+++ week-3/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\arcti\OneDrive\Documents\a.bawaallah-olufemiCOS101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\arcti\OneDrive\Documents\a.bawaallah-olufemiCOS101\week-3\practice_5\src\main.rs
